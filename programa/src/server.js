fdac0fb4d0b12f95f9cdda978e3d1cfeadd24bfc
ptrk-ap <patrickms.ap1997@outlook.com>
2026-04-24 14:24:28 -0300

    segundo commit

--- programa/src/server.js
+++ programa/src/server.js
@@ -2,7 +2,7 @@ const app = require("./app");
 const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`🔥 Servidor rodando na porta ${PORT}`);
