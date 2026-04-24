268ebd4c86bf72293693ddc911d0b1a3c8398f44
ptrk-ap <patrickms.ap1997@outlook.com>
2026-04-24 17:29:49 -0300

    banco do render

--- programa/src/database/connection.js
+++ programa/src/database/connection.js
@@ -1,13 +1,9 @@
 require("dotenv").config();
+
+
 const knex = require("knex")({
   client: "pg",
-  connection: {
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT),
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-  },
+  connection: process.env.DATABASE_URL,
   pool: { min: 2, max: 10 }
 });
 
