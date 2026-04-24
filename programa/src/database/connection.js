eb63106127753e2a8bf36932b32aaa3708b19050
ptrk-ap <patrickms.ap1997@outlook.com>
2026-04-24 18:10:17 -0300

    banco configurado url interna

--- programa/src/database/connection.js
+++ programa/src/database/connection.js
@@ -1,11 +1,18 @@
 require("dotenv").config();
-
-
 const knex = require("knex")({
   client: "pg",
-  connection: process.env.DATABASE_URL,
+  connection: {
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    ssl: { rejectUnauthorized: false }
+  },
   pool: { min: 2, max: 10 }
 });
+
+
 
 module.exports = knex;
 
